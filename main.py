6ad6b1302e6ccb26f56eb845bb197bf331ba3160
allwyn <allwyn153@gmail.com>
2025-08-26 13:20:49 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import asyncio
+import os
+
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
