74bdf5cae8f8b39366aa40b3412ad7281be3ea8c
allwyn <allwyn153@gmail.com>
2025-08-26 13:36:07 +0530

    Added llm

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import asyncio
 import os
 
 from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_mcp_adapters.tools import load_mcp_tools
-from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import create_react_agent
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash", temperature=0, google_api_key=os.getenv("GEMINI_API_KEY")
+)
+
